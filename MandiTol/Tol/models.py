2d12930cfbcdf1ae44e7b07ba02e68f08af4b799
tanmayks1999 <tanmayks1999.ts@gmail.com>
2022-03-27 15:28:21 +0530

    basic template

--- MandiTol/Tol/models.py
+++ MandiTol/Tol/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TolDiary(models.Model):
+    # user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="emergencycontacts",null=True)
+    # contact = models.EmailField()
+    kisan_name  = models.CharField(max_length=20)
+    item_name   = models.CharField(max_length=20)
+    weight      = models.IntegerField()
+    rate        = models.IntegerField()
+    bags        = models.IntegerField()
+    # phone = PhoneField()
+    objects = models.Manager()
+
+    def __str__(self):
+        return str(self.contact)
--- MandiTol/Tol/views.py
+++ MandiTol/Tol/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def start(response):
+    if (response.method == "POST"):
+        if (response.POST.get('start')):
+            return redirect('/data')
+        else :
+            return render(response, 'Tol/startTol.html', {})
+
+    return render(response, 'Tol/startTol.html', {})
+
+def data(response):
+    # if (response.method == "POST"):
+    #     if (response.POST.get('save')):
+    return render(response,"Tol/data.html",{})
