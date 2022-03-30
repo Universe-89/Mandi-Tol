d66808e4d550c4c4dc2033e79fd68e2864b59863
tanmayks1999 <tanmayks1999.ts@gmail.com>
2022-03-30 15:04:51 +0530

    carry forward

--- MandiTol/Tol/views.py
+++ MandiTol/Tol/views.py
@@ -20,26 +20,48 @@ def data(response):
             info.item_name         = response.POST.get("item_name")
             info.rate              = response.POST.get("rate")
             count                  = response.POST.get("totalGaadi")
-            countInteger = int(count)
+            countInteger = int(count) - 1
             info.bags  = 0
-            while(countInteger>0):
-                info.bags = info.bags + int(response.POST.get("bags" +count))
+            while(countInteger>=0):
+                info.bags = info.bags + int(response.POST.get("bags" + str(countInteger)))
                 countInteger = countInteger - 1
 
-
-                
-
-            
-            
-            
-            
             # count = count - 1;
 
             info.weight            = int(info.bags) * int(response.POST.get("bharti"))
             info.save()
 
             return redirect('/')
-    return render(response,"Tol/data.html",{})
+
+        if (response.POST.get('next')):
+            info                   = TolDiary()
+            info.kisan_name        = response.POST.get("name")
+            info.item_name         = response.POST.get("item_name")
+            info.rate              = response.POST.get("rate")
+            count                  = response.POST.get("totalGaadi")
+            countInteger = int(count) - 1
+            copy_count = countInteger
+            info.bags  = 0
+            while(countInteger>=0):
+                info.bags = info.bags + int(response.POST.get("bags" + str(countInteger)))
+                countInteger = countInteger - 1
+
+            # count = count - 1;
+
+            info.weight            = int(info.bags) * int(response.POST.get("bharti"))
+            info.save()
+
+            print("&&&&&&&&&&&&&&&&&&&")
+            print(copy_count)
+            last_gaddiwala_name = response.POST.get("driverName" + str(copy_count))
+            last_bags_loaded = response.POST.get("bags" + str(copy_count))
+            last_capacity = int(response.POST.get("capacity" + str(copy_count))) - int(last_bags_loaded)
+            print("&&&&&&&&&&&&&&&&&&&")
+            print(last_capacity)
+
+
+            return render(response,"Tol/data.html",{"gaddi_wala":last_gaddiwala_name,"capacity":last_capacity})
+    return render(response,"Tol/data.html",{"gaddi_wala":None,"capacity":None})
 
 def test(response):
     return render(response,"Tol/test.html",{})
