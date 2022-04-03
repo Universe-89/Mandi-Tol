d061a0fe1e9f010d094da2f50f29bbf207a017ad
tanmayks1999 <tanmayks1999.ts@gmail.com>
2022-04-03 16:43:21 +0530

    regster added

--- MandiTol/Tol/admin.py
+++ MandiTol/Tol/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import TolDiary
+from .models import *
+
 
 # Register your models here.
 class RatingAdmin(admin.ModelAdmin):
     readonly_fields = ('date_modified',)
 
 admin.site.register(TolDiary) 
+admin.site.register(Items) 
+admin.site.register(Stock_register) 
--- MandiTol/Tol/views.py
+++ MandiTol/Tol/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect,HttpResponseRedirect
 from django.http import HttpResponseRedirect
-from .models import TolDiary
+from .models import *
+from decimal import *
 
 
 # Create your views here.
@@ -8,14 +9,24 @@ def start(response):
     if (response.method == "POST"):
         if (response.POST.get('start')):
             return redirect('/data')
+
         elif (response.POST.get('TodaysTol')):
-            TolList = TolDiary.objects.all()
+            TolList = TolDiary.objects.all().order_by('-date_modified')
             return render(response,'Tol/TolDiaryData.html', {"TolList":TolList})
+
+        elif (response.POST.get('search')):
+            name = response.POST.get("search_name")
+            stock_list = Stock_register.objects.get(item_name = name)
+            return render(response,'Tol/stock.html', {"stock_list":stock_list})
+
+        elif (response.POST.get('averages')):
+            stock_list = Stock_register.objects.all()
+            return render(response,'Tol/stock.html', {"stock_list":stock_list})
 
         else :
             return render(response, 'Tol/startTol.html', {})
 
-    return render(response, 'Tol/startTol.html', {})
+    return render(response, 'Tol/stock.html', {})
 
 def data(response):
     if (response.method == "POST"):
@@ -41,6 +52,9 @@ def data(response):
 
             if(extraBharti != 0):
                 info.weight = info.weight + extraBharti
+                info.bags = info.bags + 1
+
+            updateStock(info.item_name,info.weight,info.rate,info.bags)
 
             info.save()
 
@@ -76,6 +90,9 @@ def data(response):
             if(extraBharti != 0):
                 info.weight = info.weight + extraBharti
                 last_capacity = last_capacity - 1
+                info.bags = info.bags + 1
+
+            updateStock(info.item_name,info.weight,info.rate,info.bags)
 
 
             info.save()
@@ -86,3 +103,53 @@ def data(response):
 
 def test(response):
     return render(response,"Tol/test.html",{})
+
+
+def updateStock(item_name,weight,rate,bags):
+    expenses  = Items.objects.get(item_name = item_name)
+    dhare_amount = Decimal(weight) * Decimal(rate) / Decimal(100)
+    mandi_tax = Decimal(dhare_amount) * Decimal(expenses.mandi_tax) / Decimal(100)
+    krishikalyan_ses = Decimal(dhare_amount) * Decimal(expenses.krishikalyan_ses) / Decimal(100)
+    hamali = Decimal(bags) * Decimal(12)
+    gaddi_bhada = Decimal(bags) * Decimal(5)
+    tola = Decimal(bags) * Decimal(2)
+    dhare_amount = dhare_amount + mandi_tax + krishikalyan_ses + hamali + gaddi_bhada + tola
+
+    register  = Stock_register.objects.get(item_name = item_name)    
+    amount_new = Decimal(register.total_amount) + dhare_amount
+    weight_new = Decimal(register.total_weight) + Decimal(weight)
+    average_new = amount_new / weight_new
+
+    # register.update(total_amount = amount_new,total_weight = weight_new,averge = average_new)
+    register.total_amount = amount_new
+    register.total_weight = weight_new
+    register.average = average_new
+
+    register.save()
+
+
+def query(name = None):
+    if name:
+        recipes = bhav.objects.filter(name__contains = name)
+    else:
+        recipes = bhav.objects.all()
+    return recipes
+def search(response):
+    
+    if(name == None) :
+        name = "FULLRESULT"
+
+    if name=='FULLRESULT':
+        result = query(None)
+    else:
+        result = query(name)
+
+    context = {"stock_list" : result}
+    return render(response,"frontpage/home.html",context)
+
+
+
+
+
+    
+
